9a84dfa3d4ab9405013352ac72e6f13917ad22f6
Raúl Fortea Ibáñez <133568718+Octabius141UPV@users.noreply.github.com>
2024-11-07 11:44:50 +0100

    add Vercel adapter and update output configuration in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react()],
+  output: 'server',
+  adapter: vercel()
 });
